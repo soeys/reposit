78a3adba2b8a79bf1fd8b0d300e634be716ba4ec
soeys <vadimdeniz@yahoo.com>
2020-11-12 13:17:51 +0200

    Lab5 fixed

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -1,22 +1,34 @@
-#include <stdio.h>
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include "windows.h"
 
-int main()
-{
-    double x, y;
 
-    printf("Введите х: ");
-    scanf_s("%lf", &x);
-    printf("Введите y: ");
-    scanf_s("%lf", &y);
 
-    printf("x = %lf, y = %lf \n", x, y);
+int main(void) {
+	
 
-    if (((x <= 1, y <= 1) && (x >= 0, y >= 0))
-        && ((x <= 0, y <= 0) && (x >= -1, y >= -1))
-        && ((x <= 1, y <= 0) && (x >= 0, y >= -1)))
-        printf("Точка попадает в область\n");
-    else
-        printf("Точка не попадает в область\n");
+	double x, y; 
+	
+	printf("Введите координату x >");
+	scanf("%lf", &x);
+	printf("Введите координату y >");
+	scanf("%lf", &y);
 
-    return 0;
+	printf("x=%6.3lf;  y=%6.3lf\n", x, y);
+
+		if (((x >= -1) && (x <= 1)) && ((y >= -1) && (y <= 1)))
+		{
+			if ((x <= 0) && (y >= 0))
+				printf("Точка не попадает в область\n");
+			else printf("Точка попадает в область\n");
+
+		}
+		else	printf("Точка не попадает в область\n");
+
+		printf("Для продолжения нажмите любую клавишу");
+		
+	
+
+	return 0;
 }
