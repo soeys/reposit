2e709d037f303f11dba45d8571589fe8ad9f8121
soeys <vadimdeniz@yahoo.com>
2020-11-26 11:45:02 +0200

    1

--- Lab7/Lab7/Lab7.cpp
+++ Lab7/Lab7/Lab7.cpp
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	int n, h;
+	short n, h;
 	double x, y;
 	
 	for (n = 0; n < 5; n++) {
@@ -12,29 +12,29 @@ int main(void)
 
 		for (x = 0; x < 8; x += 0.25)
 		{
-			if (x < 2) {
+			if (x < 2) 
+			{
 				y = sqrt( 1 - (x - 1) * (x - 1) );
 			}
-			else if (x < 4) {
-				y = sqrt(1 - (x - 3) * (x - 3));
+			else if (x < 4) 
+			{
+				y = sqrt(1 - (x - 3) * (x - 3) );
 			}
-			else if (x < 6) {
-				y = sqrt(1 - (x - 5) * (x - 5));
+			else if (x < 6)
+			{
+				y = sqrt(1 - (x - 5) * (x - 5) );
 			}
-			else {
+			else 
+			{
 				y = 0;
 			}
 			printf("| %5.2lf | %10.7lf |", x + n * 8, y);
 
-			h = (y + 1) * 10;
-			if (y - 1 - h * 10 > 0.5) h++;
+			h = (y + 1) * 5;
 			for (; h > 0; h--) printf(" ");
 			printf("*\n");
-
 		}
 		getchar();
-
 	}
 	return 0;
-
 }
