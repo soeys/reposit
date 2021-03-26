d5bdd9494bb4f5856bc5f434571080c546bfbf93
soeys <vadimdeniz@yahoo.com>
2021-03-26 12:31:42 +0300

    1

--- Lab 3 SM2/Lab 3 SM2/Header.h
+++ Lab 3 SM2/Lab 3 SM2/Header.h
@@ -1,3 +1,11 @@
 #pragma once
 
-void array_filling(arr[]);
+#include <iostream>
+#include <iomanip>
+#define N 10
+#define EDNLINER cout << endl << endl;
+using namespace std;
+
+void arrayprint(int* A, int n);
+void arrayfilling(int* A);
+void maxarray(int* A, int* amax, int n, int max1, int max2, int max3);
